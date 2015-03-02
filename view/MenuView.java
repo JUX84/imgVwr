68c153cb556ed151643f6524801bada3476646a7
JUX84 <mehdi.mhiri@gmail.com>
2015-03-02 15:14:55 +0100

    add about/help menu

--- view/MenuView.java
+++ view/MenuView.java
@@ -14,9 +14,11 @@ public class MenuView extends JMenuBar implements Observer {
 
 	private JMenu fileMenu;
 	private JMenu editMenu;
+	private JMenu helpMenu;
 	private JMenuItem opnImg;
 	private JMenuItem exit;
 	private JMenuItem rnmImg;
+	private JMenuItem about;
 
 	public MenuView(Controller controller) {
 		super();
@@ -25,9 +27,11 @@ public class MenuView extends JMenuBar implements Observer {
 
 		fileMenu = new JMenu();
 		editMenu = new JMenu();
+		helpMenu = new JMenu();
 		opnImg = new JMenuItem();
 		exit = new JMenuItem();
 		rnmImg = new JMenuItem();
+		about = new JMenuItem();
 
 		opnImg.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -48,15 +52,22 @@ public class MenuView extends JMenuBar implements Observer {
 				controller.imageRenamed(str);
 			}
 		});
+		about.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, language.getString("aboutContent"), language.getString("about"), JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
 
 		rnmImg.setEnabled(false);
 
 		fileMenu.add(opnImg);
 		fileMenu.add(exit);
 		editMenu.add(rnmImg);
+		helpMenu.add(about);
 
 		add(fileMenu);
 		add(editMenu);
+		add(helpMenu);
 
 		controller.init(this);
 	}
@@ -65,9 +76,11 @@ public class MenuView extends JMenuBar implements Observer {
 		this.language = language;
 		fileMenu.setText(language.getString("menuFile"));
 		editMenu.setText(language.getString("menuEdit"));
+		helpMenu.setText(language.getString("menuHelp"));
 		opnImg.setText(language.getString("menuFileOpen"));
 		exit.setText(language.getString("menuFileExit"));
 		rnmImg.setText(language.getString("menuEditRename"));
+		about.setText(language.getString("about"));
 	}
 
 	public void update(Observable o, Object arg) {
